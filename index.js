0d6fd3190a40d3c9bb079cec87c24cf32a4a6862
Daniel Neighman <dneighman@squareup.com>
2013-11-19 11:57:01 -0800

    Adds bytes and long handling

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var Compiler = require('./lib/compiler').Compiler;
-Compiler.compile("./protos/descriptors.json");
+exports.Protob    = require('./lib/protob').Protob
+exports.Compiler  = require('./lib/compiler').Compiler
+exports.Message   = require('./lib/message').Protob
+exports.Enum      = require('./lib/enum').Enum;
+exports.EnumValue = require('./lib/enum').EnumValue;
+exports.Service   = require('./lib/service').Service;
+
+exports.Compiler.compile(); // compile the base protos
